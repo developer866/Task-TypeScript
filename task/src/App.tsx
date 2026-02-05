fdfaef302f39459c035eb205e049e327181af215
Developer866 <opeyemijoseph866@gmail.com>
2026-02-05 20:28:42 +0100

    worked on adding and marking todo as done

--- task/src/App.tsx
+++ task/src/App.tsx
@@ -1,25 +1,61 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import "./App.css";
 import { InputField } from "./components/InputField";
-import { Todo } from "./components/model";
+import type { Todo } from "./components/model";
 
 const App = () => {
   const [todo, setTodo] = useState<string>("");
   const [todos, setTodos] = useState<Todo[]>([]);
+  // const{isDone,setIsDone}=useState<boolean>(false)
+  
+  const handleDone=(id:number)=>{
+    setTodos(todos.map((t)=>{
+      if(t.id===id){
+        return{...t,isDone:!t.isDone}
+      }
+      return t;
+    }))
+  }
 
   const handleAdd = (e: React.FormEvent) => {
     e.preventDefault();
+
     if (todo) {
-      setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }]);
+      setTodos([
+        ...todos,
+        {
+          id: Date.now(),
+          todo: todo,
+          isDone: false,
+        },
+      ]);
       setTodo("");
       console.log(todos);
     }
   };
+
+
   return (
     <>
       <div className="app">
         <span className="heading">Task</span>
-        <InputField setTodo={setTodo} todo={todo} handleAdd={handleAdd} />
+        <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+        <p>Open Task</p>
+        {todos.filter((t) => !t.isDone).map((t) => (
+          <div key={t.id}>
+            <p>{t.todo}</p>
+            <p>{t.id}</p>
+            <button onClick={()=>handleDone(t.id)}>Mark as {t.isDone ? "Undone":"Done"}</button>
+          </div>
+        ))}
+
+        <p>Completed list</p>
+        {todos.map((t) => (
+          <div key={t.id}>
+            {t.isDone && <p>{t.todo}</p>}
+            {t.id}
+          </div>
+        ))}
       </div>
     </>
   );
--- task/src/components/InputField.tsx
+++ task/src/components/InputField.tsx
@@ -1,21 +1,23 @@
 interface Props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
-  handleAdd:(e: React.FormEvent)=>void
+  handleAdd: (e: React.FormEvent) => void;
 }
-export const InputField = ({ todo, setTodo,handleAdd }: Props) => {
+export const InputField = ({ todo, setTodo, handleAdd }: Props) => {
   return (
     <main>
-      <input
-        type="text"
-        value={todo}
-        onChange={(e) => setTodo(e.target.value)}
-        placeholder="Enter a task"
-        className="input__box"
-      />
-      <button className="input__submit" type="submit" onClick={handleAdd}>
-        Go
-      </button>
+      <form action="">
+        <input
+          type="text"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+          placeholder="Enter a task"
+          className="input__box"
+        />
+        <button className="input__submit" type="submit" onClick={handleAdd}>
+          Go
+        </button>
+      </form>
     </main>
   );
 };
