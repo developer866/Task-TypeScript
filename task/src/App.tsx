1d29ed0c4854b2f34a7cc1547a5a93e49b3ce4fc
Developer866 <opeyemijoseph866@gmail.com>
2026-02-05 20:42:33 +0100

    Save todo to local sstorage

--- task/src/App.tsx
+++ task/src/App.tsx
@@ -1,24 +1,36 @@
-import { useState,useEffect } from "react";
+import { useState } from "react";
 import "./App.css";
 import { InputField } from "./components/InputField";
 import type { Todo } from "./components/model";
 
 const App = () => {
   const [todo, setTodo] = useState<string>("");
-  const [todos, setTodos] = useState<Todo[]>([]);
-  // const{isDone,setIsDone}=useState<boolean>(false)
-  
-  const handleDone=(id:number)=>{
-    setTodos(todos.map((t)=>{
-      if(t.id===id){
-        return{...t,isDone:!t.isDone}
-      }
-      return t;
-    }))
+  const [todos, setTodos] = useState<Todo[]>(() => {
+    const storedTodos = localStorage.getItem("todos");
+    return storedTodos ? JSON.parse(storedTodos) : [];
+  });
+
+  const savedTodos = (updatedTodos:Todo[])=>{
+    setTodos(updatedTodos);
+    localStorage.setItem('todos',JSON.stringify(updatedTodos))
   }
+
+  const handleDone = (id: number) => {
+  
+      const updatedTodos = todos.map((t) => {
+        if (t.id === id) {
+          return { ...t, isDone: !t.isDone };
+        }
+        return t;
+      });
+      savedTodos(updatedTodos)
+  
+   
+  };
 
   const handleAdd = (e: React.FormEvent) => {
     e.preventDefault();
+    if(!todo) return
 
     if (todo) {
       setTodos([
@@ -34,20 +46,23 @@ const App = () => {
     }
   };
 
-
   return (
     <>
       <div className="app">
         <span className="heading">Task</span>
         <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
         <p>Open Task</p>
-        {todos.filter((t) => !t.isDone).map((t) => (
-          <div key={t.id}>
-            <p>{t.todo}</p>
-            <p>{t.id}</p>
-            <button onClick={()=>handleDone(t.id)}>Mark as {t.isDone ? "Undone":"Done"}</button>
-          </div>
-        ))}
+        {todos
+          .filter((t) => !t.isDone)
+          .map((t) => (
+            <div key={t.id}>
+              <p>{t.todo}</p>
+              <p>{t.id}</p>
+              <button onClick={() => handleDone(t.id)}>
+                Mark as {t.isDone ? "Undone" : "Done"}
+              </button>
+            </div>
+          ))}
 
         <p>Completed list</p>
         {todos.map((t) => (
