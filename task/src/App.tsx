f21d4aeb642bdb22013cf13d7f3408c118ebf58c
Developer866 <opeyemijoseph866@gmail.com>
2026-02-06 14:30:04 +0100

    Added delete button to my task bar

--- task/src/App.tsx
+++ task/src/App.tsx
@@ -10,27 +10,39 @@ const App = () => {
     return storedTodos ? JSON.parse(storedTodos) : [];
   });
 
-  const savedTodos = (updatedTodos:Todo[])=>{
+  const totalCount = todos.length;
+  const completedCount = todos.filter((t)=>t.isDone).length
+  const openCount = todos.filter((t)=>!t.isDone).length
+  
+  const savedTodos = (updatedTodos: Todo[]) => {
     setTodos(updatedTodos);
-    localStorage.setItem('todos',JSON.stringify(updatedTodos))
-  }
+    localStorage.setItem("todos", JSON.stringify(updatedTodos));
+  };
+
+  const handleDelete = (id: number) => {
+    const updatedTodos = todos.filter((t) => t.id !== id);
+    setTodos(updatedTodos);
+    localStorage.setItem("todo", JSON.stringify(updatedTodos));
+  };
+
+  const clearTodo = () => {
+    setTodo([]);
+    localStorage.removeItem("todo");
+  };
 
   const handleDone = (id: number) => {
-  
-      const updatedTodos = todos.map((t) => {
-        if (t.id === id) {
-          return { ...t, isDone: !t.isDone };
-        }
-        return t;
-      });
-      savedTodos(updatedTodos)
-  
-   
+    const updatedTodos = todos.map((t) => {
+      if (t.id === id) {
+        return { ...t, isDone: !t.isDone };
+      }
+      return t;
+    });
+    savedTodos(updatedTodos);
   };
 
   const handleAdd = (e: React.FormEvent) => {
     e.preventDefault();
-    if(!todo) return
+    if (!todo) return;
 
     if (todo) {
       setTodos([
@@ -50,28 +62,46 @@ const App = () => {
     <>
       <div className="app">
         <span className="heading">Task</span>
+
         <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+        <p>open Task:{openCount}</p>
+        <p>finished  Task:{completedCount}</p>
+        <p>Total Count:{totalCount}</p>
         <p>Open Task</p>
         {todos
           .filter((t) => !t.isDone)
           .map((t) => (
             <div key={t.id}>
               <p>{t.todo}</p>
-              <p>{t.id}</p>
+              {/* <p>{t.id}</p> */}
               <button onClick={() => handleDone(t.id)}>
                 Mark as {t.isDone ? "Undone" : "Done"}
+              </button>
+              <button
+                onClick={() => {
+                  handleDelete(t.id);
+                }}
+              >
+                Delete
               </button>
             </div>
           ))}
 
         <p>Completed list</p>
-        {todos.map((t) => (
-          <div key={t.id}>
-            {t.isDone && <p>{t.todo}</p>}
-            {t.id}
-          </div>
-        ))}
+
+        {todos
+          .filter((t) => t.isDone)
+          .map((t) => (
+            <div key={t.id}>
+              <p>{t.todo}</p>
+
+              <button onClick={() => handleDone(t.id)}>Undo</button>
+
+              <button onClick={() => handleDelete(t.id)}>Delete</button>
+            </div>
+          ))}
       </div>
+      <button onClick={clearTodo}>Clear Todo</button>
     </>
   );
 };
