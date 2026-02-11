8b14f6ca9d1d2118076a243a70905c87b178241f
Developer866 <opeyemijoseph866@gmail.com>
2026-02-11 18:11:32 +0100

    fixed validationn

--- task/src/pages/RegisterUser.tsx
+++ task/src/pages/RegisterUser.tsx
@@ -5,14 +5,15 @@ const RegisterUser = () => {
     name: "",
     email: "",
     password: "",
-    comfirmPassword: "",
+    confirmPassword: "",
     role: "",
   });
 
-  const [error, setError] = useState<string[]>([]);
+  const [errors, setErrors] = useState<string[]>([]);
 
   const validatePassword = () => {
     const newErrors: string[] = [];
+
     if (formData.password.length < 10)
       newErrors.push("Password must be at least 10 characters long");
 
@@ -30,85 +31,106 @@ const RegisterUser = () => {
 
     if (!/[a-z]/.test(formData.password))
       newErrors.push("Password must contain at least one lowercase letter");
-    setError(newErrors);
+
+    if (formData.password !== formData.confirmPassword)
+      newErrors.push("Passwords do not match");
+
+    return newErrors;
   };
 
-  const handleChange = () => {
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>,
+  ) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    validatePassword();
-    if (formData !== formData.comfirmPassword) {
-      console.log("error invalid password");
+
+    const validationErrors = validatePassword();
+
+    if (validationErrors.length > 0) {
+      setErrors(validationErrors);
+      return;
     }
+
+    setErrors([]);
+    console.log("Form Submitted:", formData);
   };
 
   return (
-    <main>
-      <form className="gap-10" onSubmit={handleSubmit}>
-        <div>
-          <h1 className="text-center text-2xl mb-10">Registering User form</h1>
+    <form onSubmit={handleSubmit}>
+        <div><h1>Registter Users</h1></div>
+      <div>
+        <label htmlFor="name">name</label>
+        <input
+          name="name"
+          className="flex-1 px-4 py-3 text-gray-700 bg-gray-50 border
+      border-gray-300 rounded-lg focus:outline-none focus:ring-2
+      focus:ring-blue-500 focus:border-transparent placeholder-gray-400
+      transition-all duration-200"
+          onChange={handleChange}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="name">email</label>
+        <input
+          name="email"
+          type="email"
+          className="flex-1 px-4 py-3 text-gray-700 bg-gray-50 border
+        border-gray-300 rounded-lg focus:outline-none focus:ring-2
+        focus:ring-blue-500 focus:border-transparent placeholder-gray-400
+        transition-all duration-200"
+          onChange={handleChange}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="name">password</label>
+        <input
+          name="password"
+          type="password"
+          className="flex-1 px-4 py-3 text-gray-700 bg-gray-50 border
+        border-gray-300 rounded-lg focus:outline-none focus:ring-2
+        focus:ring-blue-500 focus:border-transparent placeholder-gray-400
+        transition-all duration-200"
+          onChange={handleChange}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="name">Confirm Password</label>
+        <input
+          name="confirmPassword"
+          type="password"
+          className="flex-1 px-4 py-3 text-gray-700 bg-gray-50 border
+        border-gray-300 rounded-lg focus:outline-none focus:ring-2
+        focus:ring-blue-500 focus:border-transparent placeholder-gray-400
+        transition-all duration-200"
+          onChange={handleChange}
+        />
+      </div>
+      <select name="role" onChange={handleChange}>
+        <option value="">Select Role</option>
+        <option value="user">User</option>
+        <option value="seller">Seller</option>
+        <option value="admin">Admin</option>
+      </select>
+      {errors.length > 0 && (
+        <div style={{ color: "red" }}>
+          {errors.map((err, index) => (
+            <p key={index}>{err}</p>
+          ))}
         </div>
-        <div>
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            className="flex-1 px-4 py-3 text-gray-700 bg-gray-50 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent placeholder-gray-400 transition-all duration-200"
-            placeholder="name"
-            name="name"
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="Email">Email</label>
-          <input
-            type="text"
-            className="flex-1 px-4 py-3 text-gray-700 bg-gray-50 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent placeholder-gray-400 transition-all duration-200"
-            placeholder="Email"
-            name="email"
-            onChange={handleChange}
-            typeof="email"
-          />
-        </div>
-        <div>
-          <label htmlFor="password">password</label>
-          <input
-            type="text"
-            className="flex-1 px-4 py-3 text-gray-700 bg-gray-50 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent placeholder-gray-400 transition-all duration-200"
-            placeholder="name"
-            name="password"
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="confirmPassword">Confirm password</label>
-          <input
-            type="text"
-            className="flex-1 px-4 py-3 text-gray-700 bg-gray-50 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent placeholder-gray-400 transition-all duration-200"
-            placeholder="confirm password"
-            name="confirmpassword"
-            onChange={handleChange}
-          />
-          {error ? <>there is error</> : <p>there no error</p>}
-        </div>
-        <div>
-          <label htmlFor="Role">Role</label>
-          <select name="Role" id="role">
-            <option value="user">user</option>
-            <option value="user">seller</option>
-            <option value="user">admin</option>
-          </select>
-        </div>
-        <button
-          type="submit"
-          className="px-6 py-3 bg-blue-600 text-white font-medium rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all duration-200 shadow-md hover:shadow-lg active:scale-95"
-        >
-          Submit
-        </button>
-      </form>
-    </main>
+      )}
+      <button
+        type="submit"
+        className="px-6 py-3 bg-blue-600 text-white font-medium rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all duration-200 shadow-md hover:shadow-lg active:scale-95"
+      >
+        Submit
+      </button>
+    </form>
   );
 };
 
