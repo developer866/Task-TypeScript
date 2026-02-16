68a108c33aabb3e5b762e0a20534e3ea4039b138
Developer866 <opeyemijoseph866@gmail.com>
2026-02-16 21:31:49 +0100

    Worked on logOut 🔥🔥🔥🔥

--- task/src/components/Navbar.tsx
+++ task/src/components/Navbar.tsx
@@ -1,6 +1,18 @@
+import { useDispatch, useSelector } from "react-redux";
+import type { RootState } from "../app/store";
+import { logout } from "../features/auth/authSlice";
 import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Navbar() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const token = useSelector((state: RootState) => state.auth.token);
+
+  const handleLogout = () => {
+    dispatch(logout());
+    navigate("/Login")
+  };
   return (
     <main className="flex justify-between w-[90%] mx-auto">
       <section>
@@ -15,9 +27,17 @@ function Navbar() {
         </ul>
       </section>
       {/* button */}
-      <section className="flex gap-2">
-        <NavLink to={"/Login"}>Login</NavLink>
-        <NavLink to={"/Register"}>Register</NavLink>
+      <section>
+        {token ? (
+          <button className="bg-blue-500 text-white px-4 py-2 rounded" onClick={handleLogout}>
+            Logout
+          </button>
+        ) : (
+          <section className="flex gap-2">
+            <NavLink to={"/Login"}>Login</NavLink>
+            <NavLink to={"/Register"}>Register</NavLink>
+          </section>
+        )}
       </section>
     </main>
   );
