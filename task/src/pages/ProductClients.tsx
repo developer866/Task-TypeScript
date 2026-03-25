91a5e0a6b6be5316b6ee6c17be821a3cb58f8bd2
Developer866 <opeyemijoseph866@gmail.com>
2026-03-25 02:57:06 +0100

    Fixed products Api

--- task/src/pages/ProductClients.tsx
+++ task/src/pages/ProductClients.tsx
@@ -29,7 +29,7 @@ function Shop() {
 
   const fetchProducts = async () => {
     try {
-      const response = await fetch("https://task-typescript.onrender.com/api/")
+      const response = await fetch("https://task-typescript.onrender.com/api/product")
       if (!response.ok) throw new Error("Failed to fetch products");
       const data = await response.json();
       setProducts(data.products);
